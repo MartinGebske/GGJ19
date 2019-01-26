fcb88a780e08a817227ae75d89ca952d3761e091
juloose <justin.loose@ccc-werbeagentur.de>
2019-01-26 20:55:41 +0100

    no message

---
+++ GlobalGameJam2019/Assets/Scripts/input/Orbit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Orbit : MonoBehaviour
+{
+
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(player.position.x, player.position.y + 8.0f, player.position.z + 7.0f);
+    }
+
+    void LateUpdate()
+    {
+        //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+
+        offset = Quaternion.AngleAxis(CrossPlatformInputManager.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ GlobalGameJam2019/Assets/Scripts/input/OrbitAdvanced.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAdvanced : MonoBehaviour
+{
+
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    public float height = 1f;
+    public float distance = 2f;
+
+    private Vector3 offsetX;
+
+    void Start()
+    {
+
+        offsetX = new Vector3(0, height, distance);
+    }
+
+    void LateUpdate()
+    {
+        offsetX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offsetX;
+        transform.position = player.position + offsetX;
+        transform.LookAt(player.position);
+    }
+}
