bd7f62ce7d8d2a679b1480581d8b72b658b6fb60
Felix <f.deimel@me.com>
2019-01-27 01:11:26 +0100

    EKW Take started

---
+++ GlobalGameJam2019/Assets/Scripts/PlayerTakeEKW.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTakeEKW : MonoBehaviour
+{
+    public Transform EKWParent;
+
+    public bool take()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1)) {
+            if (hit.collider.tag!="EKW") {
+                return false;
+            }
+            hit.collider.transform.parent = EKWParent;
+            hit.collider.transform.position = EKWParent.position;
+            hit.collider.transform.rotation = EKWParent.rotation;
+
+            return true;
+        }
+
+        return false;
+    }
+}
